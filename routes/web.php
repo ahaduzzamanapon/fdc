89110ca53bf29ad66b18d5bbd009143c4ecb3e4f
Md Shahajahan Ali <msacse1@gmail.com>
2025-07-02 16:23:35 +0600

    Some Modified

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ReportController;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\AttendenceController;
-use App\Http\Controllers\StudentController;
-
 
 include 'demo.php';
 /*
@@ -55,18 +52,6 @@ Route::group(['middleware' => 'auth'], function () {
 
     include 'web_builder.php';
 
-    Route::post('submit_exam_result', 'StudentController@submit_exam_result')->name('submit_exam_result');
-    Route::get('forward_to_chairman/{id}', 'StudentController@forward_to_chairman')->name('students.forward_to_chairman');
-    Route::get('chairman_approve/{id}', 'StudentController@chairman_approve')->name('students.chairman_approve');
-    Route::get('generate_certificate/{id}', 'StudentController@generate_certificate')->name('students.generate_certificate');
-
-
-
-    Route::get('students_waiting_for_district_approval', 'StudentController@students_waiting_for_district_approval')->name('students.students_waiting_for_district_approval');
-    Route::get('students_waiting_for_chairman_approval', 'StudentController@students_waiting_for_chairman_approval')->name('students.students_waiting_for_chairman_approval');
-
-    Route::get('general_students_waiting_for_district_approval', 'StudentController@students_waiting_for_district_approval')->name('general_students.students_waiting_for_district_approval');
-    Route::get('general_students_waiting_for_chairman_approval', 'StudentController@students_waiting_for_chairman_approval')->name('general_students.students_waiting_for_chairman_approval');
 
 
 
@@ -75,28 +60,7 @@ Route::group(['middleware' => 'auth'], function () {
 
     //hguigig7ig
     Route::get('get_upazilas', 'HomeController@get_upazilas')->name('get_upazilas');
-    Route::get('get_table', 'StudentController@get_table')->name('students.get_table');
-    Route::get('forwardToAssessmentCenter_modal', 'StudentController@forwardToAssessmentCenter_modal')->name('forwardToAssessmentCenter_modal');
-    Route::post('forwardToAssessmentCenter_send', 'StudentController@forwardToAssessmentCenter_send')->name('forwardToAssessmentCenter_send');
 
-    Route::get('forwardToDistrictAdmin_modal', 'StudentController@forwardToDistrictAdmin_modal')->name('forwardToDistrictAdmin_modal');
-    Route::post('forwardToDistrictAdmin_send', 'StudentController@forwardToDistrictAdmin_send')->name('forwardToDistrictAdmin_send');
-
-    Route::get('forwardToChairman_modal', 'StudentController@forwardToChairman_modal')->name('forwardToChairman_modal');
-    Route::post('forwardToChairman_send', 'StudentController@forwardToChairman_send')->name('forwardToChairman_send');
-
-    Route::get('forwardToAssessmentController_modal', 'StudentController@forwardToAssessmentController_modal')->name('forwardToAssessmentController_modal');
-    Route::post('forwardToAssessmentController_send', 'StudentController@forwardToAssessmentController_send')->name('forwardToAssessmentController_send');
-
-    Route::get('backToDistrict_modal', 'StudentController@backToDistrict_modal')->name('backToDistrict_modal');
-    Route::post('backToDistrict_send', 'StudentController@backToDistrict_send')->name('backToDistrict_send');
-
-    Route::get('approveStudent_modal', 'StudentController@approveStudent_modal')->name('approveStudent_modal');
-    Route::post('approveStudent_send', 'StudentController@approveStudent_send')->name('approveStudent_send');
-
-    Route::get('generateCertificate_modal', 'StudentController@generateCertificate_modal')->name('generateCertificate_modal');
-    Route::get('generateCertificate_send', 'StudentController@generateCertificate_send')->name('generateCertificate_send');
-    Route::get('get_competences_by_occupation', 'StudentController@get_competences_by_occupation')->name('get_competences_by_occupation');
 
 
 
@@ -111,13 +75,8 @@ Route::get('remove_all_files', 'JoshController@remove_all_files');
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('{name?}', 'JoshController@showView');
 
-Route::get('qr_details/{id}', 'StudentController@qr_details')->name('students.qr_details');
 
 
 Route::get('/upload_exell', function () {
     return view('upload_exell');
 });
-
-Route::post('/import-users', [StudentController::class, 'import'])->name('import.users');
-
-
