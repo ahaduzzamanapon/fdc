e9a262f88f63325f422b3bb5e70fbbffd6a4fec6
Shakilsss <29539341+Shakilsss@users.noreply.github.com>
2025-07-13 13:20:20 +0600

    Update leave_apply_list.blade.php

--- resources/views/leaves/leave_apply_list.blade.php
+++ resources/views/leaves/leave_apply_list.blade.php
@@ -32,7 +32,7 @@ Leaves @parent
             <div class="table-responsive">
                 <nav>
                     <div class="nav nav-tabs" id="nav-tab" role="tablist">
-                      <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">ড্রাফ্‌ট</a>
+                      <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">ড্রাফ্ট</a>
                       <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">অনুমোদন</a>
                       <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false">বাতিল</a>
                     </div>
