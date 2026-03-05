0917dbcf3a2d0f1adf6abdd1173fd6839fab4f1d
Md Shahajahan Ali <msacse1@gmail.com>
2026-03-05 11:57:03 +0600

    Some Modified

--- resources/views/layouts/leftmenu.blade.php
+++ resources/views/layouts/leftmenu.blade.php
@@ -266,12 +266,12 @@
     {{-- বুকিং --}}
     @if (can('filmApplications_table')) {{-- booking_table --}}
         <li class="nav-item">
-            <a class="nav-link {!! Request::is('producer*') || Request::is('drama-application*') ? 'active' : '' !!}"
+            <a class="nav-link {!! Request::is('producer/booking') || Request::is('producer/booking_forward_table') || Request::is('drama-application*') ? 'active' : '' !!}"
                 data-bs-toggle="collapse" href="#producerBooking" role="button" aria-expanded="false" aria-controls="hr"> <i
                     class="icon im im-icon-Calendar-4"></i> <span class="item-name"> বুকিং </span> <i
                     class="right-icon im im-icon-Arrow-Right"></i>
             </a>
-            <ul class="sub-nav collapse {!! Request::is('producer*') ? 'show' : '' !!}" id="producerBooking"
+            <ul class="sub-nav collapse {!! Request::is('producer/booking') || Request::is('producer/booking_forward_table') ? 'show' : '' !!}" id="producerBooking"
                 data-bs-parent="#sidebar-menu">
                 @if (can('film_applications_index_list'))
                     <li class="nav-item">
