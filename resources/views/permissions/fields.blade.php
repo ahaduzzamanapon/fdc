f6d5624800b71e54256aa7d751e841a8356b979e
Md Shahajahan Ali <msacse1@gmail.com>
2026-09-21 18:30:34 +0600

    Some Modified

--- resources/views/permissions/fields.blade.php
+++ resources/views/permissions/fields.blade.php
@@ -20,7 +20,7 @@
 <div class="col-md-3">
     <div class="form-group">
         {!! Form::label('cat_id', __('messages.cat_id'),['class'=>'control-label']) !!}
-        {!! Form::select('cat_id', $permission, null, ['class' => 'form-control']) !!}
+        {!! Form::select('cat_id', $categories ?? [], null, ['class' => 'form-control']) !!}
     </div>
 </div>
 
